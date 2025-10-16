0e06290e55454d98b325351cd9768b49119d40fc
Vanessa Deol <vanessa.deol@outlook.de>
2025-10-16 14:15:50 +0200

    list controller added

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,37 @@
+class ListsController < ApplicationController
+
+  # A user can see all the lists
+  def index
+    @lists = List.all
+  end
+
+   # A user can see the details of a given list and its name
+  def show
+  end
+
+  # A user can create a new list
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def new
+    @list = List.new
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :created_at, :updated_at)
+  end
+
+end
